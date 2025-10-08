204b608bb1b6d81ccf2d963aab7892d7855b437c
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-10-08 12:32:43 -0400

    10/8/2025
    AHHHHHHHHHHH

--- index.js
+++ index.js
@@ -1,8 +1,141 @@
-const OneRat = [
+const Rat = [
   {
     title: "One Rat",
     price: 20,
     instock: true,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROEyzLp2DtUVy3a2RzGu4LCZ-JcXWv3WBf-Q&s",
-  },
+    alt: "Timithy",
+},
+    {
+        title:"Two Rat",
+        price: 40,
+        instock: true,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6EWWCzPOMieBGwRjtEG5Jhf1WwAn3gl1fKw&s",
+        alt: "Ratss",
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ67PNTQWCo1H2Xr4IPzrmqe9pI-BbowPwb4Q&s",
+        alt: "Pikachu"
+    },
+    {
+        title: "USDA Choice",
+        price: 10000,
+        instock: true,
+        img: "https://bigrat.kennethng.dev/big-rat-monster.png",
+        alt: "80% Fat 20% Lean"
+    },
+    {
+        title: "Shiny Rat",
+        price: 1000,
+        instock: true,
+        img: "https://www.understandinganimalresearch.org.uk/application/files/thumbnails/page_2x/2614/1166/1135/glowing-mice.jpg",
+        alt: "Science:D"
+    },
+    {
+        title: "Totally A Rat",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+    {
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
+        title: "Weird Yellow Rat I found in the Woods",
+        price: 5,
+        instock: true,
+        img: "",
+        alt: ""
+    },
 ];
