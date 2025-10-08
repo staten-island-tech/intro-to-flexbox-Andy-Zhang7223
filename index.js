e72df3c32e0334202fca8176af8e10237ace4ad4
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-10-08 12:32:26 -0400

    10/8/2025
    AHHHHHHHHHHHHHHHHHHH

---
+++ index.js
@@ -0,0 +1,8 @@
+const OneRat = [
+  {
+    title: "One Rat",
+    price: 20,
+    instock: true,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROEyzLp2DtUVy3a2RzGu4LCZ-JcXWv3WBf-Q&s",
+  },
+];
